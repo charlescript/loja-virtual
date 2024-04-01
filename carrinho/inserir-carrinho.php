cecf9978755d3a67bae9231a5ecaf014d7149923
charlescript <rocha_charles@outlook.com>
2024-04-01 02:14:17 -0300

    corrigindo bug de promoções

--- carrinho/inserir-carrinho.php
+++ carrinho/inserir-carrinho.php
@@ -9,7 +9,8 @@
         $id_cliente = @$_SESSION['id_usuario'];
         $combo = $_POST['combo'];
 
-        $pdo->query("INSERT INTO tb_carrinho(id_usuario, id_produto, quantidade, id_venda, data, combo) values('$id_cliente', '$id_produto', '1', '0', curDate(), '$combo' )");
+        $pdo->query("INSERT INTO tb_carrinho(id_usuario, id_produto, quantidade, id_venda, data, combo) 
+                        values('$id_cliente', '$id_produto', '1', '0', curDate(), '$combo' )");
 
         //echo $combo;
         echo 'Cadastrado com Sucesso!!';
